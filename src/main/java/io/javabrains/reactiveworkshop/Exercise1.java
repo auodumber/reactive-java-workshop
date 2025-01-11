ba07ae29194b65fbdc0f813f05bc091254da98d4
auodumbar <auodumbar@cdac.in>
2025-01-11 13:53:44 +0530

    Exercise 1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -7,24 +7,27 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream().filter(number -> number < 5).forEach(System.out::println);
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream().filter(number -> number > 5).skip(1).limit(3).forEach(System.out::println);
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        int numberGreaterThan5 = StreamSources.intNumbersStream().filter(number -> number > 5).findFirst().orElse(-1);
+        System.out.println("number is " + numberGreaterThan5);
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        StreamSources.userStream().forEach(user -> System.out.println(user.getFirstName()));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
-
+        StreamSources.intNumbersStream()
+                .flatMap(i -> StreamSources.userStream().filter(u -> u.getId() == i))
+                .map(User::getFirstName)
+                .forEach(System.out::println);
     }
 
 }
